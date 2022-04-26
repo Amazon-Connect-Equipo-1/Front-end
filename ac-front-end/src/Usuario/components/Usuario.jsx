427999f62ef51e0d030510d5c43382a171912154
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2022-04-26 17:11:14 -0500

    Creacion de la base para la interfaz de usuario

---
+++ ac-front-end/src/Usuario/components/Usuario.jsx
@@ -0,0 +1,10 @@
+import "../styles/Usuario.css";
+const Usuario =(props)=>{
+    return(
+        <div className="usuario-contenedor">
+            hola
+        </div>
+
+    )
+};
+export default Usuario;
