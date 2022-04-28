4f3be747b545466cc1f4e664a30578d48ee9ec76
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2022-04-28 16:19:21 -0500

    change theme color

--- ac-front-end/src/components/Usuario/Usuario.jsx
+++ ac-front-end/src/components/Usuario/Usuario.jsx
@@ -1,28 +1,29 @@
 import "../../styles/Usuario/Usuario.css";
-import logo from"../../images/logo_bbva.png"; 
+import logo from"../../images/logo_bbva.png";
+import chatbot from "./chatbot"; 
 
 const Usuario =(props)=>{
     return(
         
         <div className="usuario-contenedor">
             <img className="usuario-imagen" src= {logo}/>
+            
             <div className="usuario-chatbot-contenedor">
                 <div className="usuario-chatbot">
                     <div>
-                    <span>
-                        <span className="usuario-chatbot">
-                        <iframe className="chatbot" src="https://d2c72ljk85v2ia.cloudfront.net/index.html" frameborder="0" allowfullscreen></iframe>
+                        
+                        <span>
+                            <span className="usuario-chatbot">
+                                <iframe className="chatbot" src="https://d2c72ljk85v2ia.cloudfront.net/index.html" frameborder="0" allowfullscreen></iframe>
+                            </span>
+                            <button className="usuario-llamada">
+                                <p className="usuario-texto">
+                                    Call
+                                </p>
+                            </button>
                         </span>
-                        <button className="usuario-llamada">
-                            <p className="usuario-texto">
-                                Call
-                            </p>
-                        </button>
-                    </span>
                     </div>
-                    
                 </div>
-                    
             </div>
         </div>
     )
---
+++ ac-front-end/src/components/Usuario/chatbot.jsx
@@ -0,0 +1,11 @@
+const chatbot =(props)=>{
+
+    return(
+        <div>
+        <span className="usuario-chatbot">
+        <iframe className="chatbot" src="https://d2c72ljk85v2ia.cloudfront.net/index.html" frameborder="0" allowfullscreen></iframe>
+        </span>
+        </div>
+    )
+};
+export default chatbot;
