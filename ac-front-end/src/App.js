7d912d95a5c7ad1982f195ee99d35bbf4a4927f8
Daniel <danielttgb44@live.com.mx>
2022-04-30 22:09:52 -0500

    Profile interface complete :+1:

--- ac-front-end/src/App.js
+++ ac-front-end/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
 import Recordings from "./components/Recordings/Recordings";
 import RecordingsCard from "./components/Recordings/RecordingsCard";
 import Usuario from "./components/Usuario/Usuario";
@@ -12,6 +13,7 @@ function App() {
       {!isUser && <Login />} */}
       <Login />
       <Recordings />
+      <Profile />
     </div>
   );
 }
---
+++ ac-front-end/src/components/Profile/Profile.js
@@ -0,0 +1,24 @@
+import "../../styles/Profile/Profile.css";
+import prfl_ic from "../../images/profile_icon.png";
+const Profile = (props) => {
+  return (
+    <div className="prfl-main-container">
+      <div className="prfl-sub-container">
+        <div className="prfl-container">
+          <img src={prfl_ic} alt="profile_ic" className="profile-ic" />
+          <div className="prfl-info-container">
+            <p className="prfl-name">Jim Halpert</p>
+            <br />
+            <p className="prfl-label">Id</p>
+            <p className="prfl-label-info">3005</p>
+            <br />
+            <p className="prfl-label">Email</p>
+            <p className="prfl-label-info">front-end@mvp.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
