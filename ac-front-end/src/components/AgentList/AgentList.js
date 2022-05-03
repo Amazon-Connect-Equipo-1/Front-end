a151d7f3492d385e905d9f5ae4669c3e3b8f1886
A01749448 <a01749448@itesm.mx>
2022-05-03 01:53:51 -0500

    Agent list interface complete :)

--- ac-front-end/src/components/AgentList/AgentList.js
+++ ac-front-end/src/components/AgentList/AgentList.js
@@ -6,6 +6,25 @@ import SingleAgent from "./SingleAgent";
 const AgentList = (props) => {
   return (
     <div className="agl-main-container">
+      <div className="agl-menu-container">
+        <button className="agl-button-main">Main</button>
+        <button className="agl-button-settings">Settings</button>
+        <div></div>
+        <div></div>
+        <div></div>
+        <input
+          className="agl-search"
+          id="agl-search"
+          type="search"
+          placeholder="Search..."
+        />
+        <select className="agl-select">
+          <option>Filter</option>
+          <option value="Name">Date</option>
+          <option value="Active Calls">Tag</option>
+          <option value="Active Calls">Status</option>
+        </select>
+      </div>
       <div className="agl-sub-container">
         <SingleAgent />
         <AllAgentList />
