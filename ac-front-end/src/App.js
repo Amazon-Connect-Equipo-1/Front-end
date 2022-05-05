5d65321a4b0e83b71f610b95ca6dd1d7403d2c42
Ariadna Guzmán <58440787+A01749373@users.noreply.github.com>
2022-05-05 02:38:25 -0500

    Dashboard quicksight

--- ac-front-end/src/App.js
+++ ac-front-end/src/App.js
@@ -31,7 +31,7 @@ function App() {
       <RecordingsVideo />
       <AgentList />
     */}
-      <AgListSet />
+      <Dashboard />
       {/* <Router>
         <Navbar />
         <Routes>
--- ac-front-end/src/components/Dashboard/Dashboard.js
+++ ac-front-end/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return <iframe
+      width="960"
+      height="720"
+      src="https://us-west-2.quicksight.aws.amazon.com/sn/dashboards/bcab7222-bf07-44e9-94ef-f78bc4115e99/views/3dfaa089-4e21-4bf8-b970-755cda0597ba">
+  </iframe>
 }
 
 export default Dashboard;
