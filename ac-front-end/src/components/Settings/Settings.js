3ffd504138bf8563e97fec99fe87d144794cf857
Daniel <danielttgb44@live.com.mx>
2022-05-11 19:29:20 -0500

    Added themes for color blindness

--- ac-front-end/src/components/Settings/Settings.js
+++ ac-front-end/src/components/Settings/Settings.js
@@ -5,16 +5,37 @@ function Settings() {
   const body = document.body;
   const lightTheme = "light";
   const darkTheme = "dark";
+  const darkTheme_Protanopia = "dark_protanopia";
+  const darkTheme_Deuteranopia = "dark_deuteranopia";
+  const darkTheme_Tritanopia = "dark_tritanopia";
+  const darkTheme_Protanomaly = "dark_protanomaly";
+  const darkTheme_Deuteranomaly = "dark_deuteranomaly";
+  const darkTheme_Tritanomaly = "dark_tritanomaly";
   let theme;
 
   if (localStorage) {
     theme = localStorage.getItem("theme");
   }
 
-  if (theme === lightTheme || theme === darkTheme) {
+  if (
+    theme === lightTheme ||
+    theme === darkTheme ||
+    theme === darkTheme_Protanopia ||
+    theme === darkTheme_Deuteranopia ||
+    theme === darkTheme_Tritanopia ||
+    theme === darkTheme_Protanomaly ||
+    theme === darkTheme_Deuteranomaly ||
+    theme == darkTheme_Tritanomaly
+  ) {
     body.classList.add(theme);
   } else {
     body.classList.add(lightTheme);
+    body.classList.add(darkTheme_Protanopia);
+    body.classList.add(darkTheme_Deuteranopia);
+    body.classList.add(darkTheme_Tritanopia);
+    body.classList.add(darkTheme_Protanomaly);
+    body.classList.add(darkTheme_Deuteranomaly);
+    body.classList.add(darkTheme_Tritanomaly);
   }
 
   const switchTheme = (e) => {
@@ -41,8 +62,16 @@ function Settings() {
             className="stngs-select"
             onChange={(i) => switchTheme(i.target.value)}
           >
-            <option value="dark">Dark Mode</option>
+            <option value="dark">Dark Mode (Default)</option>
             <option value="light">Light Mode</option>
+            <option value="dark_protanopia">Dark Mode (Protanopia)</option>
+            <option value="dark_deuteranopia">Dark Mode (Deuteranopia)</option>
+            <option value="dark_tritanopia">Dark Mode (Tritanopia)</option>
+            <option value="dark_protanomaly">Dark Mode (Protanomaly)</option>
+            <option value="dark_deuteranomaly">
+              Dark Mode (Deuteranomaly)
+            </option>
+            <option value="dark_tritanomaly">Dark Mode (Tritanomaly)</option>
           </select>
         </div>
         <div className="stngs-option">
