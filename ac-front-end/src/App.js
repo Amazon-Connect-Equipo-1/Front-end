565be32b4c40d9988baab1e30fb94ed0701f06cd
Daniel <danielttgb44@live.com.mx>
2022-05-17 16:12:47 -0500

    Bug fix of when the colors changed to white

--- ac-front-end/src/App.js
+++ ac-front-end/src/App.js
@@ -27,6 +27,61 @@ import AgentMain from "./components/AgentMain/AgentMain";
 import QualityControl from "./components/QualityControl/QualityControl";
 
 function App() {
+  const body = document.body;
+
+  const lightTheme = "light";
+  const darkTheme = "dark";
+  const darkTheme_Protanopia = "dark_protanopia";
+  const darkTheme_Deuteranopia = "dark_deuteranopia";
+  const darkTheme_Tritanopia = "dark_tritanopia";
+  const darkTheme_Protanomaly = "dark_protanomaly";
+  const darkTheme_Deuteranomaly = "dark_deuteranomaly";
+  const darkTheme_Tritanomaly = "dark_tritanomaly";
+  let theme;
+
+  const smallTxtSize = "small";
+  const mediumTxtSize = "medium";
+  const bigTxtSize = "big";
+  let txtSize;
+
+  if (localStorage) {
+    theme = localStorage.getItem("theme");
+    txtSize = localStorage.getItem("txtSize");
+  }
+
+  if (
+    txtSize === smallTxtSize ||
+    txtSize === mediumTxtSize ||
+    txtSize === bigTxtSize
+  ) {
+    body.classList.add(txtSize);
+  } else {
+    body.classList.add(smallTxtSize);
+    body.classList.add(mediumTxtSize);
+    body.classList.add(bigTxtSize);
+  }
+
+  if (
+    theme === lightTheme ||
+    theme === darkTheme ||
+    theme === darkTheme_Protanopia ||
+    theme === darkTheme_Deuteranopia ||
+    theme === darkTheme_Tritanopia ||
+    theme === darkTheme_Protanomaly ||
+    theme === darkTheme_Deuteranomaly ||
+    theme === darkTheme_Tritanomaly
+  ) {
+    body.classList.add(theme);
+  } else {
+    body.classList.add(lightTheme);
+    body.classList.add(darkTheme_Protanopia);
+    body.classList.add(darkTheme_Deuteranopia);
+    body.classList.add(darkTheme_Tritanopia);
+    body.classList.add(darkTheme_Protanomaly);
+    body.classList.add(darkTheme_Deuteranomaly);
+    body.classList.add(darkTheme_Tritanomaly);
+  }
+
   const [locale, setLocale] = useState(i18n.language);
   i18n.on("languageChanged", (lng) => setLocale(i18n.language));
 
