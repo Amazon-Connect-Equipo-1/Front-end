254c40b9744fb4c42a248d5e642b6176f7568450
Ariadna Guzmán <58440787+A01749373@users.noreply.github.com>
2022-05-25 11:20:30 -0500

    Revert "Dashboard settings"
    This reverts commit cbbbf235bb52263ae36d4a70d9df0e6b60fd4873.

--- ac-front-end/src/components/Dashboard/Dashboard.js
+++ ac-front-end/src/components/Dashboard/Dashboard.js
@@ -18,6 +18,7 @@ const Dashboard = (props) => {
           height="700"
           src="https://us-west-2.quicksight.aws.amazon.com/sn/embed/share/accounts/559202700801/dashboards/f0f0db32-74b6-4fef-8a26-89103d50737b?directory_alias=amazonconnectbancos">
         </iframe>
+        <AgentActiveCalls isActiveCalls={false} />
       </div>
       
     </div>
