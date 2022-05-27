bc04f9703628e1c7f2d60b1f415aac07851d39d6
Maikindustries <a01750145@itesm.mx>
2022-05-27 14:25:59 -0500

    Revert "update oxxo form"
    This reverts commit 83ead86f6509ebfa9be635d8616d3281f9149b88.

--- ac-front-end/src/components/AgentMain/OxxoForm.js
+++ ac-front-end/src/components/AgentMain/OxxoForm.js
@@ -8,32 +8,6 @@ import ThirdParty from "./ThirdParty";
 import { saveKeys, saveClick } from "../MonitorModule.js";
 //Creates Oxxo Form
 const OxxoForm = (props) => {
-  const SendService = () => {
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "eyJraWQiOiJ1aVNXY0k0aG0rSTE3Y0lPWE1HN3NVMUxETFRtRzN4Rm1mY2lNUk5DaThNPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIxYzc0Yjk5ZC02NWQzLTRiOWItOGZjNi1lNDc2NDViZDRiNmYiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtd2VzdC0yLmFtYXpvbmF3cy5jb21cL3VzLXdlc3QtMl9EaEttVmE3NFYiLCJjbGllbnRfaWQiOiI0dXVhNGVqdWR2N2JoMTZmbGIwc2YzZ2NyOCIsIm9yaWdpbl9qdGkiOiI2ZDI4ZmE0Mi00ZTMxLTQxM2EtODhjZC01YWFmOGM2NDE4NDAiLCJldmVudF9pZCI6IjY1Y2FhZTVmLTJkZGItNDUyMC1hY2RhLTg0NTZiZDZlODBiMSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NTM2MDA0MjYsImV4cCI6MTY1MzY4NjgyNiwiaWF0IjoxNjUzNjAwNDI2LCJqdGkiOiI1OTlhMzNlMy00YjdmLTRkYmMtYTQ3MC0yNGFkNmU1MWRhYzUiLCJ1c2VybmFtZSI6IjFjNzRiOTlkLTY1ZDMtNGI5Yi04ZmM2LWU0NzY0NWJkNGI2ZiJ9.rBtUYlflwoqzFFpAAfCZSGWGiMkPdIAIN7tmtyIbSH3h1XQqwYhefmAU4C7nUkCzEuJtzX9QXLJ2EaiQW0I1y6c9QIviiKP0ljsG6dAyjwn1JqK_EaTFxWvwE1vHDcciQVAUOQYabVh16-r4eqzP0PNTF2Cx2FIXAY5n0LQgrygPR1PiBxknyU-ih0Q1Fy3E5FICrUKLDLx-FOzJ4Fs54sit6RghVEOHuxmU5f0ZZi67X-ER57ZqRq_e2rk__BenaLph6mZChOid0kTEJfIF2SqshcY67-dMQWxrOcdFmePbAxyXmx4u6FEfSxkR1MJjeEUetBWyDVO94xdXaXO-ew", //Token de autorizacion--------
-      },
-      body: JSON.stringify({
-        service: "Oxxo",
-        service_data: {
-          client_id: 1234,
-          client: "Dwight Schrute",
-          email: "DSchrute@dunderMifflin.com",
-          cellphone: "+525530323376",
-          client_location: "Alfredo's Pizza Cafe",
-          quantity: 501,
-          account_number: 6969696969,
-        },
-      }),
-    };
-    fetch("https://35.88.250.238:8443/tps/askService", requestOptions)
-      .then((response) => response.json())
-      .then((data) => this.setState()); //Guardar los los datos
-  };
-
   const INPUT_NAME = "Oxxo form";
 
   const onSubmitHandler = (event) => {
@@ -51,7 +25,6 @@ const OxxoForm = (props) => {
             onKeyDown={saveKeys}
             onClick={() => saveClick(`${INPUT_NAME} input`)}
             className="tp-input-label"
-            id="client"
           />
         </label>
         <label className="tp-name-label">
@@ -61,7 +34,6 @@ const OxxoForm = (props) => {
             onKeyDown={saveKeys}
             onClick={() => saveClick(`${INPUT_NAME} input`)}
             className="tp-input-label"
-            id="email"
           />
         </label>
         <label className="tp-name-label">
@@ -71,7 +43,6 @@ const OxxoForm = (props) => {
             onKeyDown={saveKeys}
             onClick={() => saveClick(`${INPUT_NAME} input`)}
             className="tp-input-label"
-            id="phoneNumber"
           />
         </label>
         <label className="tp-name-label">
@@ -81,7 +52,6 @@ const OxxoForm = (props) => {
             onKeyDown={saveKeys}
             onClick={() => saveClick(`${INPUT_NAME} input`)}
             className="tp-input-label"
-            id="clientLocation"
           />
         </label>
         <label className="tp-name-label">
@@ -91,7 +61,6 @@ const OxxoForm = (props) => {
             onKeyDown={saveKeys}
             onClick={() => saveClick(`${INPUT_NAME} input`)}
             className="tp-input-label"
-            id="quantity"
           />
         </label>
         <label className="tp-name-label">
@@ -101,7 +70,6 @@ const OxxoForm = (props) => {
             onKeyDown={saveKeys}
             onClick={() => saveClick(`${INPUT_NAME} input`)}
             className="tp-input-label"
-            id="accountNumber"
           />
         </label>
         <div className="tp-submit">
