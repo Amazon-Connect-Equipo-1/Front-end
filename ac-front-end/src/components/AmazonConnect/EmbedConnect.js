b32961fecbe585f3b2ec531a537fb66759f16cc6
Erick Bustos <A01378966@itesm.mx>
2022-05-29 14:14:18 -0500

    Recording Functionality
    The recording functionality works almost properly. Bug with contact ID.
    
    Co-Authored-By: Liam Garay <65176402+A01750632@users.noreply.github.com>

--- ac-front-end/src/components/AmazonConnect/EmbedConnect.js
+++ ac-front-end/src/components/AmazonConnect/EmbedConnect.js
@@ -1,10 +1,86 @@
 import "amazon-connect-streams";
-import { useEffect, React } from "react";
-import { Component } from "spinners-react";
+import { useEffect, React, useState } from "react";
+import "../../styles/AgentMain/RecordScreen.css";
+import { useReactMediaRecorder } from "react-media-recorder";
+
+
+/*
+
+  Component that embeds the Amazon Connect Agent CPP in the application,
+  starts recording when a call starts, and stops recording when the after call work ends.
+  After stopping a recording, it uploads the recording to the corresponding s3 using
+  the contact id as name.
+
+  Authors:
+    Erick Bustos
+    Luis Zamarripa
+    Liam Garay
+    Jacqueline Zavala
+    Diego Juárez
+    Daniel García
+
+*/
+
 
 const EmbedConnect = (props) => {
+  
+  // Save the contact id (id of the call)
+  const[cid,setCid] = useState("NoContactId");
+
+  // Initialize React Media recorder
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+  } = useReactMediaRecorder({ screen: true, blobPropertyBag: {type: "video/mp4"} , onStop: uploadVideo ,askPermissionOnMount: false });
+  
+
+  // Function that will upload the recording to an S3
+  // Step 1: Call lambda function. The lambda returns a link to upload a file direclty to the S3.
+  // Step 2: Make a PUT petition using the link obtained through the lambda. We need to send the File Blob through the petition.
+
+  async function uploadVideo(blobUrl,blob) {
+    
+    // Endpoint that we will use to retrieve a link that will let us upload the recording to our s3 bucket
+    const API_ENDPOINT = "https://gmfy1qbiac.execute-api.us-west-2.amazonaws.com/default/getSignedURL"
+
+    // POST petition to lambda
+    const response = await fetch(API_ENDPOINT,{
+      method:'POST',
+      mode: 'cors',
+      body: JSON.stringify({contact_id:cid})
+    }).then(response => response.json())
+    .then(data =>{
+      console.log(data); 
+      return data; 
+    })
+    .catch(error =>{
+      console.error('Error fetching uploading URL',error);
+    })
+    console.log(response.fileName);
+
+    // Create MP4 File Blob
+    let file = await new File([blob], response.fileName, { type: 'video/mp4', lastModified: Date.now() });
+    
+    console.log('Response: ', response)
+    console.log('Uploading: ', file)
+    console.log('Uploading to: ', response.uploadURL)
+    
+    // Place PUT petition to upload the mp4 to the S3.
+    const result = await fetch(response.uploadURL, {
+      method: 'PUT',
+      body: file
+    })
+    console.log('Result: ', result)
+
+    return result;
+  }
+  
+  // Code to embed the Amazon Connect CCP 
   useEffect(() => {
     const container = document.getElementById("ccp");
+    // Initialize CCP
     // eslint-disable-next-line no-undef
     connect.core.initCCP(container, {
       ccpUrl: "https://itesm2022amazonconnect.my.connect.aws/connect/ccp-v2/", // REQUIRED
@@ -34,14 +110,39 @@ const EmbedConnect = (props) => {
       ccpSynTimeout: 3000, //optional, defaults to 1000 (ms)
       ccpLoadTimeout: 10000, //optional, defaults to 5000 (ms)
     });
-
+    
+    // Code to be executed once a call starts
     // eslint-disable-next-line no-undef
-    connect.core.onAccessDenied(function auth2() {
-      console.log("no pudiste entrar XD");
+    connect.contact(function(contact){
+      // Code to execute once an agent receives a call
+      contact.onConnected(async function(contact){
+        setCid(contact.getContactId()) ;
+        await startRecording();
+      });
+      
     });
+
+    // Agent subscriptions
+    // eslint-disable-next-line no-undef
+    connect.agent(function(agent) {
+      // On state change stop recording
+      agent.onStateChange(async function(agentStateChange) {
+        if(agentStateChange.oldState === "AfterCallWork" && agentStateChange.newState === "Available"){
+          await stopRecording();
+          //await stopRecordFunct(cid);
+        }else if(agentStateChange.oldState === "Offline" && agentStateChange.newState === "Available"){
+          //await startRecording();
+        }else if(agentStateChange.oldState === "Available" && agentStateChange.newState === "Offline"){
+          //await stopRecording();
+          
+        }
+      });
+    });
+
   }, []);
 
-  return <div id="ccp" style={{ width: "400px", height: "500px" }}></div>;
+  return (<div id="ccp" style={{ width: "400px", height: "500px" }}>
+  </div>);
 };
 
 export default EmbedConnect;
