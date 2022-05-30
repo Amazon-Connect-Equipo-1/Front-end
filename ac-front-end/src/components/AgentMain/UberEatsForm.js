deddd7411e9b7689bad6dcedf0a2460d25bb178c
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2022-05-29 21:21:01 -0500

    Added food items in uber eats form

--- ac-front-end/src/components/AgentMain/UberEatsForm.js
+++ ac-front-end/src/components/AgentMain/UberEatsForm.js
@@ -6,18 +6,43 @@ Authors:
 import "../../styles/AgentMain/ThirdParty.css";
 import ThirdParty from "./ThirdParty";
 import { saveKeys, saveClick } from "../MonitorModule.js";
-import { useTranslation } from "react-i18next";
+import { GiSodaCan } from "react-icons/gi";
+import Order from "./Order";
+import { useState } from "react";
 
 const UberEatsForm = (props) => {
   const INPUT_NAME = "Uber Eats form";
-  // Language
-  const { t } = useTranslation();
+  const [soda, setSoda] = useState(0);
+  const [chips, setChips] = useState(0);
+  const [coffee, setCoffee] = useState(0);
+  const [hotDog, setHotDog] = useState(0);
+  const [aspirin, setAspirin] = useState(0);
+  const [chocolate, setChocolate] = useState(0);
+  const sodaForm = () => {
+    setSoda += 1;
+  };
+  const chipsForm = () => {
+    setChips += 1;
+  };
+  const coffeeForm = () => {
+    setCoffee += 1;
+  };
+  const hotDogForm = () => {
+    setHotDog += 1;
+  };
+  const aspirinForm = () => {
+    setAspirin += 1;
+  };
+  const chocolateForm = () => {
+    setChocolate += 1;
+  };
+
   return (
     <div>
-      <div className="tp-title">{t("uberEats")}</div>
+      <div className="tp-title">Uber Eats Service</div>
       <form>
         <label className="tp-name-label">
-          {t("client")}
+          Client:
           <input
             type="text"
             onKeyDown={saveKeys}
@@ -26,7 +51,7 @@ const UberEatsForm = (props) => {
           />
         </label>
         <label className="tp-name-label">
-          {t("email")}
+          Email:
           <input
             type="text"
             onKeyDown={saveKeys}
@@ -35,7 +60,7 @@ const UberEatsForm = (props) => {
           />
         </label>
         <label className="tp-name-label">
-          {t("phone")}
+          Cellphone:
           <input
             type="text"
             onKeyDown={saveKeys}
@@ -44,7 +69,7 @@ const UberEatsForm = (props) => {
           />
         </label>
         <label className="tp-name-label">
-          {t("clientLocation")}
+          Client location:
           <input
             type="text"
             onKeyDown={saveKeys}
@@ -52,15 +77,24 @@ const UberEatsForm = (props) => {
             className="tp-input-label"
           />
         </label>
-        <label className="tp-name-label">
-          {t("order")}
-          <input
-            type="text"
-            onKeyDown={saveKeys}
-            onClick={() => saveClick(`${INPUT_NAME} input`)}
-            className="tp-input-label"
+        <h1>Order:</h1>
+        <div className="tp-order">
+          <Order product={"Soda"} quantity={soda} function={sodaForm} />
+          <Order product={"Chips"} quantity={chips} function={chipsForm} />
+          <Order product={"Coffee"} quantity={coffee} function={coffeeForm} />
+          <Order product={"HotDog"} quantity={hotDog} function={hotDogForm} />
+          <Order
+            product={"Aspirin"}
+            quantity={aspirin}
+            function={aspirinForm}
           />
-        </label>
+          <Order
+            product={"Chocolate"}
+            quantity={chocolate}
+            function={chocolateForm}
+          />
+        </div>
+
         <div className="tp-submit">
           <input
             type="submit"
@@ -70,7 +104,7 @@ const UberEatsForm = (props) => {
               props.onChange("Main");
               saveClick(`${INPUT_NAME} input`);
             }}
-            value={t("askForService")}
+            value="Ask for service"
             className="tp-submit-button"
           />
         </div>
