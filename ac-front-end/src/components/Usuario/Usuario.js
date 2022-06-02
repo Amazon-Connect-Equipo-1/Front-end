f2b3f37ef4289ea915ebe7cef5bb670b3ef251c3
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2022-06-02 18:00:37 -0500

    deleted Client interface

--- ac-front-end/src/components/Usuario/Usuario.js
+++ ac-front-end/src/components/Usuario/Usuario.js
@@ -5,42 +5,9 @@ Authors:
 //Import Modules
 import "../../styles/Usuario/Usuario.css";
 import logo from "../../images/logo_bbva.png";
-import Llamada from "./llamada";
-import { useState } from "react";
 
 //Verify if the user is on a Call or no
 const Usuario = (props) => {
-  const [incall, setInCall] = useState(false);
-  const [statecall, setStateCall] = useState("Call");
-  const [option, setOption] = useState("usuario-en-llamada");
-
-  const enllamada = () => {
-    setInCall(!incall);
-    if (incall === true) {
-      setStateCall("Call");
-      setOption("usuario-en-llamada");
-    }
-    if (incall === false) {
-      setStateCall("Cancel");
-      setOption("usuario-no-llamada");
-    }
-  };
-
-  return (
-    <div className="usuario-contenedor">
-      <img className="usuario-imagen" src={logo} alt="Logo BBVA" />
-
-      <div className="usuario-chatbot-contenedor">
-        <div className="usuario-chatbot">
-          <div>
-            <Llamada data={incall} />
-            <button className={option} onClick={enllamada}>
-              <p className="usuario-texto">{statecall}</p>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="usuario-contenedor"></div>;
 };
 export default Usuario;
