8f4f9a142ccb7857f40cf000352ec517e6fa51b3
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2022-06-08 22:46:14 -0500

    Update MonitorModule.js

--- ac-front-end/src/components/MonitorModule.js
+++ ac-front-end/src/components/MonitorModule.js
@@ -4,7 +4,7 @@ Authors:
 
 // This module helps to register the
 // clicks and pressed keys of the user
-
+const token = window.localStorage.getItem("token");
 export const giveTimestamp = () => {
   const timestamp = Date.now();
   const fecha = new Date(timestamp);
@@ -13,9 +13,51 @@ export const giveTimestamp = () => {
 
 export const saveKeys = (event) => {
   console.log(event.key, giveTimestamp());
-  //plus additional functionality of filtering
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", `Bearer ${token}`);
+
+  const raw = JSON.stringify({
+    key: "h,e,l,l,o",
+    agent_id: "05065c6c-844f-416c-8159-89bd798a0145",
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://backtest.bankonnect.link/keyclick/addKeystroke",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 };
 
 export const saveClick = (elementName) => {
   console.log(`Click in ${elementName}`, giveTimestamp());
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", `Bearer ${token}`);
+
+  const raw = JSON.stringify({
+    key: "login",
+    agent_id: "05065c6c-844f-416c-8159-89bd798a0145",
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("https://backtest.bankonnect.link/keyclick/addClick", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 };
