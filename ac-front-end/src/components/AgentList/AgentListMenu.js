1e23584635291073fb256d7ae4146eafba0a327e
Daniel <danielttgb44@live.com.mx>
2022-06-09 21:01:10 -0500

    Changed Input in Agent List

--- ac-front-end/src/components/AgentList/AgentListMenu.js
+++ ac-front-end/src/components/AgentList/AgentListMenu.js
@@ -8,6 +8,7 @@ import { saveKeys, saveClick } from "../MonitorModule.js";
 import { useTranslation } from "react-i18next";
 import { useContext, useState } from "react";
 import { AgentAAndQAContext } from "../AgentsAAndQASupplier";
+import { Autocomplete, TextField } from "@mui/material";
 
 const AgentListMenu = (props) => {
   const INPUT_NAME = "agent";
@@ -17,6 +18,7 @@ const AgentListMenu = (props) => {
   const [addUserMenu, setAddUserMenu] = useState(false);
   const [comment, setComment] = useState("");
   const [searchString, setSearchString] = useState("");
+  const [, selectedAgent, , sendFeedback] = useContext(AgentAAndQAContext);
   // const [, , , sendFeedback] = useContext(AgentAAndQAContext);
 
   const onChangeAddUserMenu = () => {
@@ -32,6 +34,21 @@ const AgentListMenu = (props) => {
     setSearchString(event.target.value);
   };
 
+  const options = [];
+
+  const [arrAgents] = useContext(AgentAAndQAContext);
+  {
+    arrAgents.map((agent) =>
+      options.push({ label: agent.name, id: agent.agent_id })
+    );
+  }
+
+  const [value, setValue] = useState(null);
+  const onChangeValue = (newValue) => {
+    setValue(newValue);
+  };
+
+  const [, , changeSelectedAgent] = useContext(AgentAAndQAContext);
   return (
     <div className="aglm-menu-container">
       {window.localStorage.getItem("userType") === "Admin" && (
@@ -40,7 +57,7 @@ const AgentListMenu = (props) => {
         </button>
       )}
       <div className="aglm-search-container">
-        <input
+        {/*<input
           className="aglm-search"
           id="aglm-search"
           type="search"
@@ -49,10 +66,29 @@ const AgentListMenu = (props) => {
           onKeyDown={saveKeys}
           onChange={onChangeSearchString}
           value={searchString}
+      /> */}
+        <Autocomplete
+          disablePortal
+          className="aglm-search"
+          id="aglm-search"
+          options={options}
+          value={value}
+          onClick={() => saveClick(`${INPUT_NAME} input`)}
+          onKeyDown={saveKeys}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+            //onChangeSearchString(newValue.label);
+            console.log(newValue.id);
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              className="text-field"
+              id="text-field"
+              label={t("Agents")}
+            />
+          )}
         />
-        <button href="/" className="aglm-search-btn">
-          {t("search")}
-        </button>
       </div>
     </div>
   );
