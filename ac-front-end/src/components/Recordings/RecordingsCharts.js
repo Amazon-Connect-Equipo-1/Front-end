c86e0bc60434746e79412f5c8a161b5eec7b1e6c
Maikindustries <a01750145@itesm.mx>
2022-06-13 20:53:37 -0500

    improvement in interface

--- ac-front-end/src/components/Recordings/RecordingsCharts.js
+++ ac-front-end/src/components/Recordings/RecordingsCharts.js
@@ -103,7 +103,7 @@ const RecordingsChart = (props) => {
             }}
           />
         </div>
-        <div>
+        <div className="recc-margin-top-lg">
           <Line
             style={{ height: "20rem" }}
             data={stateLine}
